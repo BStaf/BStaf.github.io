84122e72b872d30c5bf01060f5d700804d46157b
BillS <will.staf@gmail.com>
2026-02-28 20:42:24 -0500

    added bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello from Webpack!");
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+console.log("Hello from Webpack + Bootstrap!");
+
+const button = document.getElementById("myButton");
+button?.addEventListener("click", () => {
+  alert("Button clicked!");
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
 const path = require('path');
 
 module.exports = {
-entry: './src/index.ts',
-module: {
-  rules: [
-     {
-       test: /\.ts$/,
-       use: 'ts-loader',
-       exclude: /node_modules/,
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'), // output to /dist
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
-   ],
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'], // handles CSS imports
+      },
+    ],
   },
   resolve: {
     extensions: ['.ts', '.js'],
   },
+  mode: 'development', // or 'production' for production build
 };
